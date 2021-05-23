15787b0df81d320cd91ce1bd604f84d785829379
alvaro bianor <bianor.alvaro12@gmail.com>
2021-05-23 16:27:36 -0300

    Finished

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const routes = new Router();
 
 routes.use((req, res, next) => {
 
-  req.messsage = {men: 'Tudo limpo'};
+  req.message = {middleware: 'Tudo limpo'};
 
   next();
 })
@@ -19,5 +19,39 @@ routes.get('/', (req, res) => {
 
 
 })
+
+routes.get('/home/:name', (req, res) => {
+
+
+  const {name} = req.params
+
+  return res.json({...req.message, name: name || 'Alvaro'})
+
+
+})
+
+routes.get('/products', (req, res) => {
+
+
+  const {inove} = req.query
+
+
+  return res.json({...req.message, inove})
+
+
+})
+
+routes.post('/dev', (req, res) => {
+
+  const data = req.body;
+
+  console.log('Dados: ', data)
+
+
+  return res.status(201).json({message: 'Deu certo!'})
+
+
+})
+
 
 module.exports = routes;
